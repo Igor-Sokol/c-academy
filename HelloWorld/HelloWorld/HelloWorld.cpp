ba911f003f5a2e0c9b7802e83811d12e5ec19bed
Igor Sokol <i.sokol@openmygame.com>
2024-11-26 02:39:16 +0300

    module-4

--- HelloWorld/HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld/HelloWorld.cpp
@@ -2,5 +2,15 @@
 
 int main()
 {
-    std::cout << "Hello World!";
+	using namespace std;
+	setlocale(LC_ALL, "RU");
+
+	int value = 0;
+
+	cout << "Введите число" << endl;
+	cin >> value;
+
+	value > 0 && value % 2 == 0 ? value <<= 2 : value >>= 2;
+
+	cout << "Результат - " << value << endl;
 }
